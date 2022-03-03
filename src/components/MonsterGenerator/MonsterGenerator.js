3231d97988ec4b3ddcfee9d206786e2fce3ea3f4
acourney <acourney@ucsc.edu>
2022-03-03 14:15:12 -0800

    fetch data to randomize a monster with random challenge rating

--- src/components/MonsterGenerator/MonsterGenerator.js
+++ src/components/MonsterGenerator/MonsterGenerator.js
@@ -1,9 +1,72 @@
 import React from "react";
+import { DataContext } from "../../dataContext";
+import { useState, useContext, useEffect } from "react";
 
 function MonsterGenerator(props) {
+  const { formState, setFormState } = useContext(DataContext);
+
+  const [monsterData, setMonsterData] = useState({});
+  const [isLoading, setLoading] = useState(true);
+
+  const challenge_rating = formState.partyLevel;
+  let URL = "";
+
+  const randomPage = Math.floor(Math.random() * 22);
+
+  formState.partyLevel
+    ? (URL = `https://api.open5e.com/monsters/?challenge_rating=${challenge_rating}`)
+    : (URL = `https://api.open5e.com/monsters/?page=${randomPage}`);
+
+  console.log(URL);
+
+  useEffect(() => {
+    fetch(URL)
+      .then((res) => res.json())
+      .then((data) => {
+        setMonsterData(data);
+        console.log(data);
+        setLoading(false);
+        // console.log(raceData.results);
+      })
+      .catch((err) => console.error(`Oops, something went wrong: ${err}`));
+  }, []);
+
   return (
     <main>
       <p>Hello from MonsterGenerator</p>
+      {isLoading ? (
+        <p>Generating Info...</p>
+      ) : (
+        <div className="displayNPCInfo">
+          <p>
+            <span>Monster Type: </span>
+            {
+              monsterData.results[
+                Math.floor(Math.random() * monsterData.results.length)
+              ].name
+            }
+          </p>
+          <p>
+            <span>Challenge Rating: </span>
+            {
+              monsterData.results[
+                Math.floor(Math.random() * monsterData.results.length)
+              ].challenge_rating
+            }
+          </p>
+          <p>
+            <span>HP: </span>
+            {
+              monsterData.results[
+                Math.floor(Math.random() * monsterData.results.length)
+              ].hit_points
+            }
+          </p>
+          <p>
+            <span>Abilities: </span>
+          </p>
+        </div>
+      )}
     </main>
   );
 }
