80d015d35825f1e1056a3d1300bdb8205dee4b61
acourney <acourney@ucsc.edu>
2022-03-07 11:02:04 -0800

    fix npc generation with specific class

--- src/components/GenerateNPCByClass/SpecificClassNPC.js
+++ src/components/GenerateNPCByClass/SpecificClassNPC.js
@@ -3,9 +3,14 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 function SpecificClassNPC(props) {
+  let randRaceIndex = 0;
+
   const { npcClass } = useParams();
   const [npcInfo, setNpcInfo] = useState([]);
+  const [randomRace, setRandomRace] = useState({});
+
   const [isLoading, setLoading] = useState(true);
+
   const URL = `https://api.open5e.com/classes/?name=${npcClass}`;
   const raceURL = "https://api.open5e.com/races/";
 
@@ -138,8 +143,8 @@ function SpecificClassNPC(props) {
 
   useEffect(() => {
     Promise.all([
-      fetch(URL).then((res) => res.json()),
       fetch(raceURL).then((res) => res.json()),
+      fetch(URL).then((res) => res.json()),
     ])
       .then((data) => {
         setNpcInfo(data);
@@ -152,17 +157,58 @@ function SpecificClassNPC(props) {
   }, []);
 
   function handleClick() {
-    Promise.all([
-      fetch(URL).then((res) => res.json()),
-      fetch(raceURL).then((res) => res.json()),
-    ])
-      .then((data) => {
-        setNpcInfo(data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    console.log("you clicked generate new npc button");
+
+    if (isLoading === false) {
+      randRaceIndex = Math.floor(Math.random() * npcInfo[0].results.length);
+      setRandomRace(npcInfo[0].results[randRaceIndex]);
+    }
+  }
+
+  useEffect(() => {
+    if (isLoading === false) {
+      randRaceIndex = Math.floor(Math.random() * npcInfo[0].results.length);
+      setRandomRace(npcInfo[0].results[randRaceIndex]);
+    }
+  }, [isLoading]);
+
+  function chooseName(randomRace) {
+    switch (randomRace) {
+      case "Human":
+        return names["Human"][
+          Math.floor(Math.random() * names["Human"].length)
+        ];
+      case "Dwarf":
+        return names["Dwarf"][
+          Math.floor(Math.random() * names["Dwarf"].length)
+        ];
+      case "Elf":
+        return names["Elf"][Math.floor(Math.random() * names["Elf"].length)];
+      case "Halfling":
+        return names["Halfling"][
+          Math.floor(Math.random() * names["Halfling"].length)
+        ];
+      case "Dragonborn":
+        return names["Dragonborn"][
+          Math.floor(Math.random() * names["Dragonborn"].length)
+        ];
+      case "Gnome":
+        return names["Gnome"][
+          Math.floor(Math.random() * names["Gnome"].length)
+        ];
+      case "Half-Elf":
+        return names["Half-Elf"][
+          Math.floor(Math.random() * names["Half-Elf"].length)
+        ];
+      case "Half-Orc":
+        return names["Half-Orc"][
+          Math.floor(Math.random() * names["Half-Orc"].length)
+        ];
+      case "Tiefling":
+        return names["Tiefling"][
+          Math.floor(Math.random() * names["Tiefling"].length)
+        ];
+    }
   }
 
   return (
@@ -173,33 +219,15 @@ function SpecificClassNPC(props) {
         <div className="displayNPCInfo">
           <p>
             <span>Name: </span>
-            {
-              names[
-                npcInfo[1].results[
-                  Math.floor(Math.random() * npcInfo[1].results.length)
-                ].name
-              ][
-                npcInfo[0].results[
-                  Math.floor(Math.random() * npcInfo[0].results.length)
-                ].name.length
-              ]
-            }
+            {chooseName(randomRace.name)}
           </p>
           <p>
             <span>Race: </span>
-            {
-              npcInfo[1].results[
-                Math.floor(Math.random() * npcInfo[1].results.length)
-              ].name
-            }
+            {randomRace.name}
           </p>
           <p>
             <span>Languages: </span>
-            {
-              npcInfo[1].results[
-                Math.floor(Math.random() * npcInfo[1].results.length)
-              ].languages
-            }
+            {randomRace.languages}
           </p>
           <p>
             <span>Class: </span>
